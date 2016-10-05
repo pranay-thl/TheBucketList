59ea9578f63f372943af4bdc24531640b842babf
Pranay Pandey <pranay.pnd@gmail.com>
2016-10-05 18:45:21 +0530

    db test

--- checkDb.php
+++ checkDb.php
@@ -4,8 +4,10 @@
 <?php
 $MONGODB_URL="mongodb://admin:ZGXWEDVVLBXOPZNQ@sl-us-dal-9-portal.3.dblayer.com:15536/admin";
 $m = new MongoClient($MONGODB_URL);
+$url = parse_url($MONGODB_URL);
+$db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
 echo "connected";
-$db = $m->admin;
+$db = $m->selectDB($db_name);
 echo "databse selected";
 $m->close();
 ?>
