da1ce22feadb5a5d90ed478af8685c79e8ca6075
Pranay Pandey <pranay.pnd@gmail.com>
2016-10-05 18:53:48 +0530

    db test

--- checkDb.php
+++ checkDb.php
@@ -2,7 +2,7 @@
 <html>
 <body>
 <?php
-$MONGODB_URL="mongodb://admin:ZGXWEDVVLBXOPZNQ@sl-us-dal-9-portal.3.dblayer.com:15536/admin";
+$MONGODB_URL="mongodb://admin:ZGXWEDVVLBXOPZNQ@sl-us-dal-9-portal.3.dblayer.com:15536/admin?ssl=true";
 $m = new MongoClient($MONGODB_URL);
 $url = parse_url($MONGODB_URL);
 $db_name = preg_replace('/\/(.*)/', '$1', $url['path']);
